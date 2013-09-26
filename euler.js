9d209f65acd14162ac979fe5bdef17f43c441d24
Rasmus Pedersen <isenhard@gmail.com>
2013-09-26 13:47:11 +0200

    Euler 3 solved

--- euler.js
+++ euler.js
@@ -12,6 +12,7 @@ function euler2() {
 	var sum = 0,
 		i = 2,
 		prev = 1;
+
 	while(i<4e6) {
 		var current = i;
 		if(i%2 == 0) {
@@ -22,4 +23,18 @@ function euler2() {
 	}
 	return sum;
 }
-console.log(euler2());
+
+function euler3(n) {
+	if(!n) {
+		var n = 600851475143;
+	}
+	var max = Math.round(Math.sqrt(n));
+
+	for(var i=max; i>1; i--) {
+		if(n%i == 0 && !euler3(i)) {
+			return i;
+		}
+	}
+	return false;
+}
+console.log(euler3());
